f54a32487fa60dd49d30d36e83baeafbf72bb66d
Michielo <michiel.kamphuis@hetnet.nl>
2024-05-03 21:32:07 +0200

    cleanup

--- evaluation/main.py
+++ evaluation/main.py
@@ -1,20 +1,64 @@
 import argparse
-from evaluate import Evaluate
+from metrics.Metric import Metric
+import models.impl.Michielo.mt5__small_en__nl_translation
+import models.impl.Michielo.mt5__small_nl__en_translation
+import models.impl.facebook.nllb__200__3___3B
+
+
+model_dict = {
+    "michielo_en-nl": models.impl.Michielo.mt5__small_en__nl_translation.Model("Michielo/mt5-small_en-nl_translation"),
+    "michielo_nl-en": models.impl.Michielo.mt5__small_nl__en_translation.Model("Michielo/mt5-small_nl-en_translation"),
+    "nllb_3.3B": models.impl.facebook.nllb__200__3___3B.Model("facebook/nllb-200-3.3B")
+}
 
 
 def main():
-    
-    parser = argparse.ArgumentParser(description="Select the type of translation model.")
+    parser = argparse.ArgumentParser(description="Select the type of translation model and evaluation metrics.")
 
-    parser.add_argument("--model", choices=["en-nl", "nl-en"], required=True,
-                        help="The type of translation model (en-nl or nl-en).")
-    
+    parser.add_argument("--model", choices=list(model_dict.keys()), required=True,
+                        help="The type of translation model.")
+
+    parser.add_argument("--bleu", action="store_true",
+                        help="Include BLEU score in the evaluation.")
+
+    parser.add_argument("--chrf", action="store_true",
+                        help="Include chrF score in the evaluation.")
+
+    parser.add_argument("--chrf++", action="store_true", dest="chrf_plus_plus",
+                        help="Include chrF++ score in the evaluation.")
+
+    parser.add_argument("--all", action="store_true",
+                        help="Include all evaluation metrics (BLEU, chrF, chrF++).")
+
     args = parser.parse_args()
 
-    print(f"Model '{args.model}' selected.")
+    metrics = []
+    if args.all:
+        metrics = [Metric.BLEU, Metric.CHRF, Metric.CHRF_PLUS_PLUS]
+    elif args.bleu:
+        metrics.append(Metric.BLEU)
+    elif args.chrf:
+        metrics.append(Metric.CHRF)
+    elif args.chrf_plus_plus:
+        metrics.append(Metric.CHRF_PLUS_PLUS)
+    else:
+        print("You did not select any evaluation metrics!")
+        print("Please run with one or more of the following: '--bleu' '--chrf' '--chrf++' '--all'")
+        print("For more help, please run with --help")
+        exit(0)
 
-    eval = Evaluate(args.model)
-    eval.evaluate_model()
+    print(f"Model '{args.model}' selected.")
+    if metrics:
+        print(f"Evaluation metric(s): {', '.join(metric.name for metric in metrics)}")
+
+    model = model_dict[args.model]
+
+    print('Loading model...')
+    model.load_model()
+    print('Preparing data...')
+    model.prep()
+    print('Running evaluation...')
+    model.run_eval(metrics)
     
 
 if __name__ == "__main__":
